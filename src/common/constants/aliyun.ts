ce71e2bad83c227c3c46e5cf09f58189f8e32e04
llbeOk <102140798+llbeOk@users.noreply.github.com>
2023-07-01 19:38:23 +0800

    s删除敏感信息

--- src/common/constants/aliyun.ts
+++ src/common/constants/aliyun.ts
@@ -1,5 +1,6 @@
-export const SIGN_NAME = '黑石博客';
-export const TEMPLATE_CODE = 'SMS_266600014';
-export const ACCESS_KEY = 'LTAI5t6D9W6Arg5ZwEYfWqjG';
-export const ACCESS_KEY_SECRET = '8YNGXLtXXTOIR6pp7P4kH963FmTOnt';
-export const JWT_SECRET = 'blackstone';
+//敏感信息
+export const SIGN_NAME = '';
+export const TEMPLATE_CODE = '';
+export const ACCESS_KEY = '';
+export const ACCESS_KEY_SECRET = '';
+export const JWT_SECRET = '';
